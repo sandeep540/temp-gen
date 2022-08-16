ff5518c392cb6f1581214dfaa468df19ea7fc6b4
Sandeep Kongathi <kn.sandeep@gmail.com>
2022-08-16 18:37:49 +0530

    updated

--- readme.md
+++ readme.md
@@ -8,3 +8,5 @@ Change the Broker info in Python file, regenrate the docker
 
     kcat -C -b localhost:51209,localhost:51214,localhost:51215 -t temperatures -o beginning
 
+    set env KAFKA_BROKERS=host.docker.internal:51209,host.docker.internal:51214,host.docker.internal:51215
+
--- tempgen.py
+++ tempgen.py
@@ -5,12 +5,14 @@ import random
 import uuid
 from kafka import KafkaProducer
 from json import dumps  
+import os
 
 def get_data():
 
     # Producer instance
     my_data = {'temperature' : random.randint(1, 100), 'id' : str(uuid.uuid4())}
-    prod = KafkaProducer(bootstrap_servers=['localhost:51209','localhost:51214','localhost:51215'],value_serializer = lambda x:dumps(x).encode('utf-8'))
+    brokers = os.environ.get('KAFKA_BROKERS').split(',')
+    prod = KafkaProducer(bootstrap_servers=brokers,value_serializer = lambda x:dumps(x).encode('utf-8'))
     print(my_data)
     prod.send(topic='temperatures', value=my_data)
     prod.flush()
